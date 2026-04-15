176ac067bcbd01a3db3eb9eabb6ce6367ac584b1
Fighter-55 <fmayer06@gmail.com>
2026-04-15 15:32:05 +0200

    Create assignment1.py

---
+++ assignments/week1/assignment1.py
@@ -0,0 +1,15 @@
+print("""
+          .-------------------.
+         /--'--.------.------/|
+         |Kodak|__Ll__| [==] ||
+         |     | .--. | '''' ||
+         |     |( () )|      ||
+    jgs  |     | `--' |      |/
+         `-----'------'------'
+""")
+
+'''
+The ASCII artwork was made by Joan G. Stark (Spunk). It represents an Kodak Film Camera. 
+This Artwork was found on the website www.asciiart.eu under this link: https://www.asciiart.eu/electronics/cameras
+I liked the Artwork because it represents my enthusiasm for old retro cameras And the glorious medium of photosensitive Film I very enjoy looking at.
+'''
